8a8e0a63962b2d260cc6e47f3847ea9c66c9bce2
pyo-sh <pygosky@gmail.com>
2020-01-12 14:55:14 +0900

    4-2~4-6강

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,10 +2,14 @@ import React from 'react';
 import Head from 'next/head';
 import PropTypes from 'prop-types';
 import AppLayout from '../components/AppLayout'
+
 import { Provider } from 'react-redux';
 import reducer from '../reducers';
 import withRedux from 'next-redux-wrapper';
 import { createStore, compose, applyMiddleware } from 'redux';
+
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 
 const NodeBird = ({ Component, store }) => {
     return(
@@ -28,15 +32,18 @@ NodeBird.propTypes = {
 
 export default withRedux((initialState, options) => {
     // 여기에다가 store 커스터마이징
-    const middlewares = [];
-    //const enhancer = compose(applyMiddleware(...middlewares));
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware];
 
-    const enhancer = compose(
-        applyMiddleware(...middlewares),
-        !options.isServer && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ != 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-        );
+    // 배포할 때에는 devtools를 사용하지 않아야 하기 때문에 env.NODE_ENV를 통해 배포용인지 확인
+    const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : compose(
+      applyMiddleware(...middlewares),
+      !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+    );
 
     const store = createStore(reducer, initialState, enhancer);
-
+    sagaMiddleware.run(rootSaga);
     return store;
 })(NodeBird);
